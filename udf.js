71d7f9e1da1a858fb526f156c19930d31bc13932
Vishal Bulbule <143475073+vishal-bulbule@users.noreply.github.com>
2025-01-08 09:08:39 +0530

    Update udf.js

--- udf.js
+++ udf.js
@@ -1,5 +1,11 @@
 function transform(line) {
     var values = line.split(',');
+
+    // Check if the line is the header by checking the first value
+    if (values[0].toLowerCase() === 'id') {
+        return null; // Ignore the header row
+    }
+
     var obj = new Object();
     obj.id = values[0];
     obj.name = values[1];
@@ -8,4 +14,4 @@ function transform(line) {
     obj.city = values[4];
     var jsonString = JSON.stringify(obj);
     return jsonString;
-   }
+}
